fc2569b90592a2425386770600b8db0b52f3657a
logoiny <lz95awm@gmail.com>
2021-04-17 20:54:37 +0800

    definiation modle

---
+++ modle/captcha.go
@@ -0,0 +1,32 @@
+package modle
+
+type CaptchaGet struct {
+	ImgHigh    int  `json:"img_high"`
+	ImgWidth   int  `json:"img_width"`
+	DfWh       bool `json:"df_wh"`
+}
+
+type CaptchaGetRsp struct {
+	Buf       string `json:"buf"`
+	CaptchaId string `json:"captcha_id"`
+}
+
+type CaptchaReload struct {
+	CaptchaId string `json:"captcha_id"`
+	ImgHigh    int  `json:"img_high"`
+	ImgWidth   int  `json:"img_width"`
+	DfWh       bool `json:"df_wh"`
+}
+
+type CaptchaReloadRsp struct {
+	Buf       string `json:"buf"`
+}
+
+type CaptchaVerify struct {
+	CaptchaId string `json:"captcha_id"`
+	Captcha string `json:"work"`
+}
+
+type CaptchaVerifyRsp struct {
+	Passed bool `json:"passed"`
+}
